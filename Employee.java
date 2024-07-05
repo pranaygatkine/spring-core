5bfa08dd2e99194b6b7019f7f842c065502df7b6
Pranay Gatkine <139122694+pranaygatkine@users.noreply.github.com>
2024-07-05 17:34:02 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,12 +1,24 @@
 package com.mainapp;
 
 public class Employee {
+	
+	private int eid;
+	private String ename;
 
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
 	private Employee() {
 		System.out.println("EMP INST");
 		
 	}
 	public void info() {
 		System.out.println("EMP INFO>>>>>>>>>>>>>");
+		System.out.println("EID=" +eid);
+		System.out.println("ENAME=" +ename);
+
 	}
 }
--- Launch.java
+++ Launch.java
@@ -1,34 +1,24 @@
 package com.mainapp;
 
-import java.util.Scanner;
+
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Launch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-           Scanner sc = new Scanner(System.in);
 		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
 		
 
-		System.out.println("enter key");
-		int key=sc.nextInt();
+	
+		Employee emp1 = (Employee) ac.getBean("emp");
 		
-		Employee emp = (Employee) ac.getBean("emp");
-		emp.info();
+		emp1.setEid(1000);
+		emp1.setEname("Pranay");
+		emp1.info();
 		
-		Manager man = (Manager) ac.getBean("man");
-		man.info();
-		
-		if(key==1212) {
-			Student std = (Student) ac.getBean("std");
-			std.info();
-		}
-		else
-		{
-			System.out.println("Wrong User");
-		}
-		
+		Employee emp2 = (Employee) ac.getBean("emp");
+		emp2.info();
 		
 	}
 
