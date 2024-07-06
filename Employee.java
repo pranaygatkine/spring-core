7b4aac4cbdd7f58b63fa5613caffef7db68b95e1
Pranay Gatkine <139122694+pranaygatkine@users.noreply.github.com>
2024-07-06 18:28:16 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,30 +1,23 @@
 package com.mainapp;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class Employee implements InitializingBean,DisposableBean{
-	static {
-		System.out.println("BEAN LOADING");
-	}
-	private Employee() {
+public class Employee {
+	
+	public Employee() {
 		System.out.println("BEAN INSTANTIATION");
 	}
 	
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("RES-ALL: init");	}
-	
+	public static Employee getObj(){
+		
+		System.out.println("BUS_LOGIC");
+		return new Employee();
+		
+		
+	}
+
      public void info() {
 		System.out.println("CUSTOM METHOD");
 		
 	}
-	@Override
-	public void destroy() throws Exception {
-		System.out.println("RES-DEALL: Destroy");
-		
-	}
-	
 	
 }
