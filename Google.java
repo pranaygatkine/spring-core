2dfcf0a72859af510fe766618e583e73222a2680
Pranay Gatkine <139122694+pranaygatkine@users.noreply.github.com>
2024-07-18 15:47:25 +0530

    Add files via upload

--- Google.java
+++ Google.java
@@ -7,17 +7,19 @@ public class Google {
 	private int id;
 	private String headOffice;
 	private List<String> listOfServiceCountry;
-	public Google(int id, String headOffice, List<String> listOfServiceCountry) {
+	private String extra;
+	public Google(int id, String headOffice, List<String> listOfServiceCountry, String extra) {
 		super();
 		this.id = id;
 		this.headOffice = headOffice;
 		this.listOfServiceCountry = listOfServiceCountry;
-		
-		
+		this.extra = extra;
 	}
 	@Override
 	public String toString() {
 		return "Google [id=" + id + ", headOffice=" + headOffice + ", listOfServiceCountry=" + listOfServiceCountry
-				+ "]";
+				+ ", extra=" + extra + "]";
 	}
+	
+	
 }
